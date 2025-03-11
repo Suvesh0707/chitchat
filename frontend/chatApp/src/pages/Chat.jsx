b4cf15304f2d18c1dfc855c38a8495da6be15485
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-03-12 00:28:38 +0530

    path error for profile photo

--- frontend/chatApp/src/pages/Chat.jsx
+++ frontend/chatApp/src/pages/Chat.jsx
@@ -173,7 +173,7 @@ function Chat() {
                                         onKeyDown={(e) => e.key === "Enter" && handleSendMessage()}
                                     />
                                     <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600" onClick={handleSendMessage}>
-                                        Send
+                                        post
                                     </button>
                                 </div>
                             </div>
